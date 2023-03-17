387dba23048b96ce732925cd6e65f01ee5569b65
Alankar Tamgadge <alankar_tamgadge@vistasoftwares.com>
2023-03-17 13:46:08 +0530

    added pagination

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ export default class Navbar extends Component {
     render() {
         return (
             <>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                     <div className="container-fluid">
                         <a className="navbar-brand" href="/">NewsMonk</a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -25,10 +25,10 @@ export default class Navbar extends Component {
                                 </li>
 
                             </ul>
-                            <form className="d-flex">
+                            {/* <form className="d-flex">
                                 <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                                 <button className="btn btn-outline-success" type="submit">Search</button>
-                            </form>
+                            </form> */}
                         </div>
                     </div>
                 </nav>
